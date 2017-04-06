f02673db5aec18a45e9c48cb600b6623106c0087
mscale92 <scalemary@gmail.com>
2017-04-05 20:34:11 -0700

    made a new folder to better organize files as well as a convert string program

---
+++ tutorial1/string_converter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace ConvertToStringApp
+{
+   class NotStringDataTypes
+    {
+        //member variables
+        bool truth;
+        int wholeNum;
+        float decimalNum;
+        
+        //value for our data, void because it does not return any one value
+        public void DataValues()
+        {
+            truth = true;
+            wholeNum = 45;
+            decimalNum = 18.9f;
+        }
+        
+        //function that prints our data on the console
+        public void Print()
+        {
+            Console.WriteLine("These are my strings");
+            Console.WriteLine(truth.ToString());
+            Console.WriteLine(wholeNum.ToString());
+            Console.WriteLine(decimalNum.ToString());
+        }
+        
+    }
+
+    class stringConverter
+    {
+        static void Main(string[] args)
+        {
+            NotStringDataTypes data = new NotStringDataTypes();
+            //assign our other class to a variable in order to access our functions and data
+
+            data.DataValues();
+            //assign value to our data values
+
+            data.Print();
+            //print our data as converted strings
+
+            Console.ReadKey();
+        }
+    }
+}
