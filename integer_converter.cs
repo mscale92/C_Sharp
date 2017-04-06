499b5f629ac5b487d57898dd754829fca513c33a
mscale92 <scalemary@gmail.com>
2017-04-05 18:50:06 -0700

    converted floats and doubles to integers

---
+++ integer_converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace TypeConversionApp
+{
+
+class IntConversion
+    {
+        static void Main(string[] args)
+        {
+            float myfloat = 123.475f;
+            //always include f after declaring a value as a float
+            double mydouble = 4561.78984;
+            
+
+            Console.WriteLine("My float {0}", myfloat);
+            Console.WriteLine("My float converted {0}", Convert.ToInt32(myfloat));
+            Console.WriteLine("My double {0}", mydouble);
+            Console.WriteLine("My double converted {0}", Convert.ToInt32(mydouble));
+            Console.ReadKey();
+
+            //Don't forget the keyword Convert before Converting anything except strings, that's ToString();
+            
+        }
+    }
+
+}
+
+
