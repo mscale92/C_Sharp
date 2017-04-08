0ee4ea82a54b4553923a94c004bb2876d0d8fd59
mscale92 <scalemary@gmail.com>
2017-04-07 19:51:32 -0700

    made a program that shows variable definition and a program that asks for user input from the console

---
+++ tutorial1/name_of_user.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace WhatIsYourName
+{
+    public class QuestionForTheUser
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("What is your name?");
+
+            //ReadLine accepts input from the user, this is from the Console
+            name = Console.ReadLine();
+           
+
+            Console.WriteLine("Your name is {0}!", name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tutorial1/variable_def.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace VariableDefinition
+{
+    public class VariableDefProgram
+    {
+        static void Main(string[] args)
+        {
+            //declaring variables and data types
+            short a;
+            int b;
+            double c;
+
+            string A;
+            string B;
+            string C;
+
+            //assigning values to variables
+            a = 4;
+            b = 20;
+            c = a + b;
+
+            A = "4";
+            B = "20";
+            C = A + B;
+
+            //The {0} refer to the variable order after the quotes, like an array starts at zero
+            //This prevents having to break quotes, so {0} refers to variable a in this instance
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", A, B, C);
+
+            Console.ReadLine();
+
+        }
+    }
+}
